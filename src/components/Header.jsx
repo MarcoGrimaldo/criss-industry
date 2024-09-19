258ff2af2c347dd015f8c7b5299c194820ffaae7
Marco Grimaldo <mgrimdev@gmail.com>
2024-09-18 21:16:04 -0600

    init comit

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+// Header.js
+import React from "react";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+
+const Header = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          TexTech Industries
+        </Typography>
+        <Button color="inherit">Home</Button>
+        <Button color="inherit">Products</Button>
+        <Button color="inherit">About</Button>
+        <Button color="inherit">Contact</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
